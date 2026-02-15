83b40969104e8745a87fc9fc2d2c80a0fcc7a437
Nadine Squires <quantumquantara@gmail.com>
2026-02-15 07:36:29 -0500

    Create spacetime_lattice.py

---
+++ spacetime_lattice.py
@@ -0,0 +1,6 @@
+def model_lattice(input_data):
+    # From coherence_spacetime_lattice: Scalable modeling
+    import numpy as np
+    # Simple lattice sim with numpy
+    lattice = np.array([1, 2, 3])  # Placeholder array
+    return input_data
